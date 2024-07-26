2c0210fdda43a7b87dbd1c0708803c46d186c398
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-07-26 16:59:40 +0200

    fix: generate component code

--- server/index.mjs
+++ server/index.mjs
@@ -13,8 +13,44 @@ export default async function (request, response, next) {
     return next();
   }
 
-  const buffer = await file.text();
+  const source = await file.json();
+  const { js, html, meta = {} } = source;
+  const { name = 'x-invalid'} = meta;
+
+  const moduleSource = `
+  ${js}
+
+  customElements.get('${name}') || customElements.define('${name}', class extends HTMLElement {
+    connectedCallback() {
+      if (typeof onInit !== 'undefined') {
+        onInit.apply(this);
+      }
+
+      if (typeof onChange !== 'undefined') {
+        const observer = new MutationObserver(function(mutation) {
+          if (mutation.type === 'attributes') {
+            onChange.call(this, mutation.attributeName);
+          }
+        });
+
+        observer.observe(this, { attributes: true });
+        this.__obs__ = observer;
+      }
+    }
+
+    disconnectedCallback() {
+      if (typeof onDestroy !== 'undefined') {
+        onDestroy.apply(this);
+      }
+
+      if (this.__obs__) {
+        this.__obs__.disconnect();
+      }
+    }
+  })
+  `
+
   response.setHeader('Content-Type', 'application/javascript');
-  response.setHeader('Content-Length', String(buffer.length));
-  response.end(buffer);
+  response.setHeader('Content-Length', String(moduleSource.length));
+  response.end(moduleSource);
 }
