0ba86f19b4e9db8eb33faae25c25a4fd60ac1b40
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-07-31 13:51:38 +0200

    fix: export component bindings

--- index.ts
+++ index.ts
@@ -6,5 +6,6 @@ Object.assign(main, { styles });
 
 export const component = defineCustomElement(main);
 export const name = "__component__name__";
+export * from './main.vue';
 
 customElements.get(name) || customElements.define(name, component);
